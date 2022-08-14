65c1446e5a3b998398d970c42f2f8140806fc20d
Jean Leal <64782576+Jeanll7@users.noreply.github.com>
2022-08-14 10:12:38 -0300

    Add files via upload

---
+++ src/components/AboutContainer.jsx
@@ -0,0 +1,13 @@
+
+
+const AboutContainer = () => {
+  return (
+    <section className="about-container">
+      <h2>Sobre</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet temporibus quia quisquam, voluptatibus sunt aspernatur numquam ipsum eaque dolorem.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet temporibus quia quisquam, voluptatibus sunt aspernatur numquam ipsum eaque dolorem.</p>
+    </section>
+  )
+}
+
+export default AboutContainer
