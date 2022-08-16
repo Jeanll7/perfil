e69b965e628c5a7cba386363ed9831ff577716cd
Jean Leal <64782576+Jeanll7@users.noreply.github.com>
2022-08-16 00:36:51 -0300

    project

--- src/components/ProjectsContainer.jsx
+++ src/components/ProjectsContainer.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
 const ProjectsContainer = () => {
   return (
-    <div>ProjectsContainer</div>
+    <section className="projects-container">
+      <h2>Projetos</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Recusandae libero incidunt repellat obcaecati? Consectetur, reprehenderit. Ullam fugit deserunt quibusdam ipsa harum veniam adipisci repellendus quidem?        
+      </p>
+      <a href="#github" className="btn">
+        Ver Projeros
+      </a>
+    </section>
   )
 }
 
