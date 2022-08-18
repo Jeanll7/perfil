bd0ac8e8b4deea299ba12b297fddf3bc6affd486
Jean Leal <jeancarloleall@gmail.com>
2022-08-18 15:34:06 -0300

    text

--- src/components/AboutContainer.jsx
+++ src/components/AboutContainer.jsx
@@ -1,11 +1,9 @@
-
-
 const AboutContainer = () => {
   return (
     <section className="about-container">
       <h2>Sobre</h2>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet temporibus quia quisquam, voluptatibus sunt aspernatur numquam ipsum eaque dolorem.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet temporibus quia quisquam, voluptatibus sunt aspernatur numquam ipsum eaque dolorem.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet temporibus quia quisquam, voluptatibus sunt.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci amet temporibus quia quisquam, voluptatibus.</p>
     </section>
   )
 }
